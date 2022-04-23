30c0195775e6e3e5f5546fdc1ee8d4bae9186dfa
carlocordova21 <carlo_andre21@hotmail.com>
2022-04-23 11:31:25 -0500

    correct register

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -17,14 +17,12 @@ class UserController extends Controller
             'name' => 'required|string|max:255',
             'email' => 'required|string|email|max:255|unique:users',
             'password' => 'required|string|min:8',
-            'device_name' => 'required|string',
         ]);    
         
         $user = User::create([
             'name' => $validated['name'],
             'email' => $validated['email'],
             'password' => Hash::make($validated['password']),
-            'device_name' => $validated['device_name'],
         ]);
 
         $token = $user->createToken('auth_token')->plainTextToken;
